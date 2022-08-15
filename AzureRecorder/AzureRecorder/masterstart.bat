dfbc61616ff6490fb9954425f1a6c190f4ceff3c
paige-marie <81829573+paige-marie@users.noreply.github.com>
2022-08-15 14:58:39 -0600

    changed kinect setting in start bats

--- AzureRecorder/AzureRecorder/masterstart.bat
+++ AzureRecorder/AzureRecorder/masterstart.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Program Files\Azure Kinect SDK v1.4.1\tools
-k4arecorder.exe --device 0 --external-sync master --imu OFF -e -8 -r 5 %1
+k4arecorder.exe --device 0 --external-sync master --imu OFF 5 %1
