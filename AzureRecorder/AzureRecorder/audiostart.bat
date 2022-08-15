21c22ee4afb5f9e422db54aeaf5f9d9fcd26c0b7
paige-marie <81829573+paige-marie@users.noreply.github.com>
2022-08-15 14:47:27 -0600

    add 3 bat files for audio and device numbers

---
+++ AzureRecorder/AzureRecorder/audiostart.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\Program Files\fmedia"
+::fmedia --list-dev
+fmedia --record --out="C:\Users\pmhansen\Desktop\kinect test files\audio.wav" --dev-capture=3
---
+++ AzureRecorder/AzureRecorder/getKinectDevices.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Program Files\Azure Kinect SDK v1.4.1\tools
+k4arecorder.exe --list
